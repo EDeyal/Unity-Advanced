75547f1077fa7a56e6e1175fb906dcda408bec44
DorShemTov <83244591+DorShemTov@users.noreply.github.com>
2023-06-25 21:45:38 +0300

    half way there

--- Advanced Unity Eyal Dor Ron On/Assets/Scripts/MoveToGoalAgent.cs
+++ Advanced Unity Eyal Dor Ron On/Assets/Scripts/MoveToGoalAgent.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class MoveToGoalAgent : Agent
 {
+    [SerializeField] private Transform targetTransform;
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+        float moveSpeed = 1f;
+        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
     }
 }
