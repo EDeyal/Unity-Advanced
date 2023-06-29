c128755e0b9eee90aaa7c6bd5b02d5ca543693b3
On <ontamam@gmail.com>
2023-06-29 11:29:56 +0300

    firebase

---
+++ Advanced Unity Eyal Dor Ron On/Assets/Firebase/Scripts/DatabaseManager.cs
@@ -0,0 +1,25 @@
+﻿using Firebase.Database;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    string userID;
+    DatabaseReference reference;
+    void Start()
+    {
+        userID = SystemInfo.deviceUniqueIdentifier;
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    //send from ui 
+    public void CreateUser(string name, int score)
+    {
+        User newUser = new User(name, score);
+        string json = JsonUtility.ToJson(newUser);
+
+        reference.Child("users").Child(userID).SetRawJsonValueAsync(json);
+    }
+
+}
---
+++ Advanced Unity Eyal Dor Ron On/Assets/Firebase/Scripts/User.cs
@@ -0,0 +1,15 @@
+public class User
+{
+    public string username;
+    public int score;
+
+    public User()
+    {
+    }
+
+    public User(string username, int score)
+    {
+        this.username = username;
+        this.score = score;
+    }
+}
